c6951d46498ac8b87c376605e43cbf0442a9b20f
Itaru <itaru.sugimoto@gmail.com>
2019-10-01 17:38:48 +0900

    first

---
+++ CellTest/CellTest/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  CellTest
+//
+//  Created by sugimoto.itaru on 2019/10/01.
+//  Copyright © 2019 sugimoto.itaru. All rights reserved.
+//
+
+import UIKit
+
+@available(iOS 13.0, *)
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ CellTest/CellTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CellTest
+//
+//  Created by sugimoto.itaru on 2019/10/01.
+//  Copyright © 2019 sugimoto.itaru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
